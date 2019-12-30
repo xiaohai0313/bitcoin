c448689a16daa0355049e3e4d16a5665cf38c98e
Xueling <xlmeng@asu.edu>
2019-12-29 23:05:40 -0800

    Final Version

--- index.js
+++ index.js
@@ -15,17 +15,17 @@ app.post("/",function(req,res){
     
     Digital = req.body.Digital;
     Real = req.body.Real;
+    amount = req.body.amount;
+
+    var base_url = "https://apiv2.bitcoinaverage.com/convert/global?from=";
+    var finalURL = base_url + Digital + "&to=" + Real +"&amount" + amount;
     
-
-    var base_url = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
-    var finalURL = base_url + Digital + Real;
-
     request(finalURL,function(error,response,body){
         var data = JSON.parse(body);
-        var price = data.last;
+        var price = data.price;
 
-        
-
+        var currenttime = data.time;
+        res.write("<p>The current date is " + currenttime + "</p>");
         res.write("The Current Price for " + Digital + " to " + Real + " is : " + price);
         res.send();
     })
