f3ea4b11ead557eeda3ecfede3306f3d51e56bfd
Xueling <xlmeng@asu.edu>
2019-12-29 22:19:09 -0800

    Server Start

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 
 
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/index.html");
+})
 
+app.post("/",function(req,res){
 
-
+})
 
 app.listen(3000,function(){
     console.log("Server is listen");
